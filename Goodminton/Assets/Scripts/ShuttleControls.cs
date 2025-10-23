a5dd145ad60433065fb0d7a19a68a9df6f01f0fd
Justin Ward <jw4566c@gre.ac.uk>
2025-10-23 19:08:22 +0100

    test line renderer moves with touch position
    for some reason, the initial position doesnt change unless I click off the game and back onto it

--- Goodminton/Assets/Scripts/ShuttleControls.cs
+++ Goodminton/Assets/Scripts/ShuttleControls.cs
@@ -10,6 +10,8 @@ public class ShuttleControls : MonoBehaviour
     Vector2 initTouchPos;
     Vector2 currTouchPos;
 
+    LineRenderer line;
+
     private void OnEnable()
     {
         touchAction = new TouchAction();
@@ -17,6 +19,16 @@ public class ShuttleControls : MonoBehaviour
 
         touchAction.General.TouchPosition.started += Touch_started;
         touchAction.General.TouchPosition.performed += Touch_performed;
+        touchAction.General.TouchPosition.canceled += TouchPosition_canceled;
+
+    }
+
+    private void TouchPosition_canceled(InputAction.CallbackContext ctx)
+    {
+        line.enabled = false;
+
+        initTouchPos = Vector2.zero;
+        currTouchPos = Vector2.zero;
     }
 
     private void Touch_started(InputAction.CallbackContext ctx)
@@ -28,8 +40,9 @@ public class ShuttleControls : MonoBehaviour
     private void Touch_performed(InputAction.CallbackContext ctx)
     {
         currTouchPos = ctx.ReadValue<Vector2>();
-        Debug.Log($"Current touch: {currTouchPos}");
+        //Debug.Log($"Current touch: {currTouchPos}");
 
+        line.enabled = true;        
     }
 
 
@@ -39,5 +52,24 @@ public class ShuttleControls : MonoBehaviour
         touchAction.General.TouchPosition.performed -= Touch_performed;
 
         touchAction.Disable();
+    }
+
+
+    private void Start()
+    {
+        line = GetComponentInChildren<LineRenderer>();
+        line.enabled = false;
+    }
+
+    private void Update()
+    {
+        Vector3 a = new(initTouchPos.x, initTouchPos.y, 10);
+        Vector3 b = new(currTouchPos.x, currTouchPos.y, 10);
+
+        Vector3 pos0 = Camera.main.ScreenToWorldPoint(a);
+        Vector3 pos1 = Camera.main.ScreenToWorldPoint(b);
+
+        line.SetPosition(0, pos0);
+        line.SetPosition(1, pos1);
     }
 }
